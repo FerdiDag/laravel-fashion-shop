f876f86b8fe805a4b840490b5df04acd8060be95
Ferdinando <ferdi.dagostino@gmail.com>
2020-07-07 10:43:00 +0200

    push

--- resources/views/clothes.blade.php
+++ resources/views/clothes.blade.php
@@ -1,24 +1,38 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Marca</th>
-                        <th scope="col">Distributore</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($lista_vestiti as $vestito)
-                    <tr>
-                        <td>{{ $vestito->id }}</td>
-                        <td>{{ $vestito->marca }}</td>
-                        <td>{{ $vestito->distributore }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{asset ('css/app.css')}}">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Marca</th>
+                            <th scope="col">Distributore</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($lista_vestiti as $vestito)
+                        <tr>
+                            <td>{{ $vestito->id }}</td>
+                            <td>{{ $vestito->marca }}</td>
+                            <td>{{ $vestito->distributore }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
+
+</body>
+
+</html>
